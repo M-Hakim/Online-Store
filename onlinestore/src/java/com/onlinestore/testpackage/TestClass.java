b3e8908a9e38f27ddef78fa62be6509735e395ae
M-Hakim <mahmoud.abdelhakim9610@gmail.com>
2020-03-31 22:00:32 +0200

    adding files structure

---
+++ onlinestore/src/java/com/onlinestore/testpackage/TestClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.onlinestore.testpackage;
+
+/**
+ *
+ * @author THE PR!NCE
+ */
+public class TestClass {
+    
+}
