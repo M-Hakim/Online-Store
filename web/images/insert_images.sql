2b63df935db203f9ac561121ca35c7418404a480
marwa1121 <marwaelsayed1121@gmail.com>
2020-04-09 19:41:29 +0200

    registeration page modified and header of html pages

---
+++ web/images/insert_images.sql
@@ -0,0 +1,9 @@
+/*\i 'D:/ITI/Web app development/Project/Online-Store/onlinestore/web/images/insert_images.sql'*/
+insert into products (product_name, quantity, category_id, description, price, imgurl) values ('ay 7aga', 1, 1, 'ay 7aga', 20.5, 'images/product_1.jpg');
+insert into products (product_name, quantity, category_id, description, price, imgurl) values ('ay 7aga', 1, 1, 'ay 7aga', 20.5, 'images/product_2.jpg');
+insert into products (product_name, quantity, category_id, description, price, imgurl) values ('ay 7aga', 1, 1, 'ay 7aga', 20.5, 'images/product_3.jpg');
+insert into products (product_name, quantity, category_id, description, price, imgurl) values ('ay 7aga', 1, 1, 'ay 7aga', 20.5, 'images/product_4.jpg');
+insert into products (product_name, quantity, category_id, description, price, imgurl) values ('ay 7aga', 1, 1, 'ay 7aga', 20.5, 'images/product_5.jpg');
+insert into products (product_name, quantity, category_id, description, price, imgurl) values ('ay 7aga', 1, 1, 'ay 7aga', 20.5, 'images/product_6.jpg');
+insert into products (product_name, quantity, category_id, description, price, imgurl) values ('ay 7aga', 1, 1, 'ay 7aga', 20.5, 'images/product_7.jpg');
+insert into products (product_name, quantity, category_id, description, price, imgurl) values ('ay 7aga', 1, 1, 'ay 7aga', 20.5, 'images/product_8.jpg');
