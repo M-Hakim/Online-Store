1bc176781a366daae0c3405dbfc7f3ed84a7d631
mostafarawy <mostafa.elrawwy@yahoo.com>
2020-04-16 02:56:24 +0200

    updating dynamic header and footer and logout

---
+++ onlinestore/src/java/com/onlinestore/servlets/logout.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.onlinestore.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author omega
+ */
+public class logout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       
+        if (req.getSession(false) !=null)
+        {
+         HttpSession session = req.getSession(false);
+        if (session.getAttribute("users")!=null)
+        {
+            session.setAttribute("users", null);
+        }
+        if (session.getAttribute("products")!=null)
+        {
+            session.setAttribute("products", null);
+        }
+        
+        Cookie [] cookie ;
+        cookie= req.getCookies();
+            for (Cookie cookie1 : cookie) {
+                cookie1.setMaxAge(0);
+                resp.addCookie(cookie1);
+            }
+        session.invalidate();
+        
+        }
+       
+        resp.sendRedirect("./customer/sign_in.html");
+    
+    }
+
+   
+    }
+
+    
+    
+    /**
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+      
+    
+    
+
